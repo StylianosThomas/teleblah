41323f290d5e1a5458595db38353ca460ae70610
Stylianos Thomas <stylianos.thomas@gmail.com>
2021-01-05 17:17:35 +0000

    client should be working

---
+++ server/models/blog.js
@@ -0,0 +1,37 @@
+const { db } = require ('../db/config')
+const SQL = require("sql-template-strings");
+
+class Blog {
+    constructor(data){
+        this.id = data.id
+        this.title = data.title
+        this.author = data.author
+        this.content = data.content
+    }
+
+    static create(title, author, content) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let blogData = await db.run(SQL`INSERT INTO blogs (title, author, content) VALUES (${title}, ${author}, ${content}) RETURNING *;`);
+                let newBlog = new Blog(blogData.rows[0]);
+                resolve (newBlog);
+            } catch (error) {
+                reject('Error creating new blog')
+            }
+        });
+    }
+
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let blogData = await db.run(SQL`SELECT * FROM blogs WHERE id = ${id};`);
+                let blog = new Blog(blogData.rows[0]);
+                resolve (blog);
+            } catch (error) {
+                reject('Blog not found');
+            }
+        });
+    }
+}
+
+module.exports = Blog;
